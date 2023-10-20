5f351783bc1fa6563171a81f8db7743884f782df
Octavillt <ovillafrancot@gmail.com>
2023-10-20 00:28:05 -0600

    Se agrega gitignore y nuevo ejercicio en Estructuras de Control

--- prueba.js
+++ prueba.js
@@ -1,5 +1,7 @@
 /*
-Se le proporciona una matriz (que tendrá una longitud de al menos 3, pero podría ser muy grande) que contiene números enteros. La matriz está completamente compuesta por enteros impares o completamente compuesta por enteros pares excepto por un solo número entero N. Escriba un método que tome la matriz como argumento y devuelva este "valor atípico" N.
+Se le proporciona una matriz (que tendrá una longitud de al menos 3, pero podría ser muy grande) que contiene números enteros. 
+La matriz está completamente compuesta por enteros impares o completamente compuesta por enteros pares excepto por un 
+solo número entero N. Escriba un método que tome la matriz como argumento y devuelva este "valor atípico" N.
 */
 const arrNums = [2, 4, 0, 100, 4, 11];
 let sumaElemt = 0;
